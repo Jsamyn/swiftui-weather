e179e7d007847da3628aa053d6455f8ff9702937
joseph.samyn@cvo.com <joseph.samyn@cvo.com>
2022-05-11 20:03:30 -0700

    Initial Commit

---
+++ WTHR/WTHR/WTHRApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTHRApp.swift
+//  WTHR
+//
+//  Created by Joe Samyn on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WTHRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
